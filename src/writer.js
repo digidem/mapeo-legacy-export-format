21ce5cddf77fe8363611258ba2b9995a1314b592
Evan Hahn <me@evanhahn.com>
2024-11-20 04:06:33 +0000

    chore: run Prettier

--- src/writer.js
+++ src/writer.js
@@ -1,75 +1,76 @@
-import fs from "node:fs";
-import {pEvent} from 'p-event'
-import { readdir, readFile } from "node:fs/promises";
-import { join, relative, basename, format, dirname} from "node:path";
-import crypto from "hypercore-crypto";
-import Multifeed from "multifeed";
-import { ZipFile } from "yazl";
+import fs from 'node:fs'
+import { pEvent } from 'p-event'
+import { readdir, readFile } from 'node:fs/promises'
+import { join, relative, basename, format, dirname } from 'node:path'
+import crypto from 'hypercore-crypto'
+import Multifeed from 'multifeed'
+import { ZipFile } from 'yazl'
 
 /**
  * @param {String} srcPath path to `kappa.db` folder
  * @param {String} [destPath] path file to save
  * @returns {Promise<String>} OUT
  */
-export default async function MLEFWriter(srcPath, destPath = "output") {
-  const zip = new ZipFile();
-  const multi = Multifeed(srcPath, { valueEncoding: "json" });
-  const FEEDS_PATH = "docs";
-  const EXTENSION = "mlef";
-  const outPath = destPath === 'output' ? join(dirname(srcPath), destPath) : destPath
-  const OUT = format({ name: outPath , ext: EXTENSION });
-  await multiReady(multi);
-  const zipFile = fs.createWriteStream(OUT);
+export default async function MLEFWriter(srcPath, destPath = 'output') {
+  const zip = new ZipFile()
+  const multi = Multifeed(srcPath, { valueEncoding: 'json' })
+  const FEEDS_PATH = 'docs'
+  const EXTENSION = 'mlef'
+  const outPath =
+    destPath === 'output' ? join(dirname(srcPath), destPath) : destPath
+  const OUT = format({ name: outPath, ext: EXTENSION })
+  await multiReady(multi)
+  const zipFile = fs.createWriteStream(OUT)
   const onClose = pEvent(zip.outputStream.pipe(zipFile), 'close')
 
-  const media = writeAttachments(srcPath);
-  const feeds = writeFeeds(multi);
+  const media = writeAttachments(srcPath)
+  const feeds = writeFeeds(multi)
   for await (const { file, filename } of media) {
-    zip.addBuffer(file, join(basename(srcPath), filename));
+    zip.addBuffer(file, join(basename(srcPath), filename))
   }
   for await (const { doc, filename } of feeds) {
     zip.addBuffer(
       JSON.stringify(doc, null, 4),
-      join(basename(srcPath), FEEDS_PATH, filename),
-    );
+      join(basename(srcPath), FEEDS_PATH, filename)
+    )
   }
 
-  zip.end();
+  zip.end()
   await onClose
-  return OUT;
+  return OUT
 }
 
 async function* writeAttachments(srcPath) {
-  const mediaDir = await readdir(join(srcPath, "media"), {
+  const mediaDir = await readdir(join(srcPath, 'media'), {
     recursive: true,
     withFileTypes: true,
-  });
+  })
   for (const fileOrDir of mediaDir) {
     if (fileOrDir.isFile()) {
-      const file = await readFile(join(fileOrDir.parentPath, fileOrDir.name));
+      const file = await readFile(join(fileOrDir.parentPath, fileOrDir.name))
       const filename = join(
         relative(srcPath, fileOrDir.parentPath),
-        fileOrDir.name,
-      );
-      yield { filename, file };
+        fileOrDir.name
+      )
+      yield { filename, file }
     }
   }
 }
 
 async function* writeFeeds(multi) {
   for (const core of multi.feeds()) {
-    const stream = core.createReadStream();
+    const stream = core.createReadStream()
     for await (const doc of stream) {
-      const version = doc.version?.split("@")[1];
-      if (!doc.id) throw new Error("no doc.id on doc");
+      const version = doc.version?.split('@')[1]
+      if (!doc.id) throw new Error('no doc.id on doc')
       // TODO: use better default version than '_'
       // since that first version of doc doesn't haver a version, and second is 0?
-      const filename = `${doc.id}@${version || "_"}.json`;
+      const filename = `${doc.id}@${version || '_'}.json`
       try {
-        doc.migrationMetadata = await addMigrationMetadata(core);
-        yield { doc, filename };
+        doc.migrationMetadata = await addMigrationMetadata(core)
+        yield { doc, filename }
       } catch (e) {
-        console.error("error creating migration metadata", e);
+        console.error('error creating migration metadata', e)
       }
     }
   }
@@ -80,11 +81,11 @@ async function* writeFeeds(multi) {
  */
 async function addMigrationMetadata(core) {
   return {
-    rootHashChecksum: crypto.tree(await getRootHash(core)).toString("hex"),
-    signature: (await getCoreSignature(core)).toString("hex"),
-    coreKey: core.key.toString("hex"),
+    rootHashChecksum: crypto.tree(await getRootHash(core)).toString('hex'),
+    signature: (await getCoreSignature(core)).toString('hex'),
+    coreKey: core.key.toString('hex'),
     blockIndex: core.length,
-  };
+  }
 }
 
 /**
@@ -96,9 +97,9 @@ function multiReady(multi) {
     // TODO: ready cb returns two params: a num and a function...
     multi.ready(() => {
       //if(err) return reject(err)
-      resolve();
-    });
-  });
+      resolve()
+    })
+  })
 }
 
 /**
@@ -112,11 +113,11 @@ function getCoreSignature(core) {
        *  @param {{signature: Buffer}} sig
        */
       (err, sig) => {
-        if (err) return reject(err);
-        resolve(sig.signature);
-      },
-    );
-  });
+        if (err) return reject(err)
+        resolve(sig.signature)
+      }
+    )
+  })
 }
 
 /**
@@ -132,9 +133,9 @@ function getRootHash(core) {
        * @param {Buffer} roots
        */
       (err, roots) => {
-        if (err) return reject(err);
-        resolve(roots);
-      },
-    );
-  });
+        if (err) return reject(err)
+        resolve(roots)
+      }
+    )
+  })
 }
--- test/writer.js
+++ test/writer.js
@@ -1,19 +1,19 @@
-import test from "node:test";
-import assert from "node:assert/strict";
+import test from 'node:test'
+import assert from 'node:assert/strict'
 import fs from 'node:fs/promises'
 import { join, relative, basename } from 'node:path'
-import { json } from 'node:stream/consumers';
+import { json } from 'node:stream/consumers'
 import yauzl from 'yauzl-promise'
-import mime from 'mime/lite';
-import MLEFWriter from "../src/writer.js";
+import mime from 'mime/lite'
+import MLEFWriter from '../src/writer.js'
 
 // add testing of links of docs to attachments
 // 1. if there's a ref, check that file exists (on the three folders?)
 // 2. check that there is no image unreferenced? (not that important)
 
-test("loading and packing a db into a zip", async (t) => {
+test('loading and packing a db into a zip', async (t) => {
   const dbName = 'db1'
-  const path = new URL(join('./fixture', dbName), import.meta.url).pathname;
+  const path = new URL(join('./fixture', dbName), import.meta.url).pathname
 
   const out = await MLEFWriter(path, path)
   let zip = await yauzl.open(out)
@@ -23,30 +23,40 @@ test("loading and packing a db into a zip", async (t) => {
   const zipDefault = await yauzl.open(outDefaultPath)
   assert(zipDefault, 'mlef file should exist on default path')
 
-  const attachments = (await fs.readdir(join(path, 'media'), {recursive:true, withFileTypes:true}))
-  .filter(p => p.isFile())
-  .map(p => relative(path,join(p.path, p.name)))
+  const attachments = (
+    await fs.readdir(join(path, 'media'), {
+      recursive: true,
+      withFileTypes: true,
+    })
+  )
+    .filter((p) => p.isFile())
+    .map((p) => relative(path, join(p.path, p.name)))
 
-  const dataPromises = (await fs.readdir(path, {recursive: true, withFileTypes:true}))
-  // read all files called 'data', which contain json lines
-  .filter(p => join(p.path,p.name).match(/data/))
-  .map(async (p) => await fs.readFile(join(p.path, p.name)))
+  const dataPromises = (
+    await fs.readdir(path, { recursive: true, withFileTypes: true })
+  )
+    // read all files called 'data', which contain json lines
+    .filter((p) => join(p.path, p.name).match(/data/))
+    .map(async (p) => await fs.readFile(join(p.path, p.name)))
 
-  const docs = (await Promise.all(dataPromises)).toString('utf-8')
-  .split('\n')
-  .filter(s => s.length !== 0)
-  .map((s) => JSON.parse(s))
+  const docs = (await Promise.all(dataPromises))
+    .toString('utf-8')
+    .split('\n')
+    .filter((s) => s.length !== 0)
+    .map((s) => JSON.parse(s))
 
   const attachmentsInZip = []
   const docsInZip = []
 
-  for await (const entry of zip){
+  for await (const entry of zip) {
     const path = relative(dbName, entry.filename)
-    if(path.includes('media')){
+    if (path.includes('media')) {
       attachmentsInZip.push(path)
     }
-    if(path.includes('docs')){
-      const {migrationMetadata, ...doc} = await json(await entry.openReadStream())
+    if (path.includes('docs')) {
+      const { migrationMetadata, ...doc } = await json(
+        await entry.openReadStream()
+      )
       docsInZip.push(doc)
     }
   }
@@ -57,16 +67,15 @@ test("loading and packing a db into a zip", async (t) => {
     'the file tree of attachments zipped matches the original'
   )
 
-  assert.deepEqual(
-    docsInZip,
-    docs,
-    'the docs zipped matches the original'
-  )
+  assert.deepEqual(docsInZip, docs, 'the docs zipped matches the original')
 
-  const attachmentsRef = docsInZip.flatMap(doc => doc.attachments)
+  const attachmentsRef = docsInZip.flatMap((doc) => doc.attachments)
   const attachmentsInZipAsRefs = attachmentsInZip
-  .filter(attachment => attachment.match(/original/))
-  .map(attachment => ({ id: basename(attachment), type: mime.getType(attachment) }))
+    .filter((attachment) => attachment.match(/original/))
+    .map((attachment) => ({
+      id: basename(attachment),
+      type: mime.getType(attachment),
+    }))
 
   assert.deepEqual(
     new Set(attachmentsRef),
@@ -79,12 +88,12 @@ test("loading and packing a db into a zip", async (t) => {
     zip.close()
     zipDefault.close()
   })
-});
+})
 
-async function teardown(toRemove){
-  Promise.all(toRemove.map(async (path) => {
-    await fs.rm(path)
-  }))
+async function teardown(toRemove) {
+  Promise.all(
+    toRemove.map(async (path) => {
+      await fs.rm(path)
+    })
+  )
 }
-
-
